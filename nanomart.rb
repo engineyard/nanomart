39d9a24d87b12a1d7253dd9c906d5abe3e64e867
Sam Merritt <spam@andcheese.org>
2010-09-09 16:13:00 -0700

    rename Item#nam to Item#name

--- nanomart.rb
+++ nanomart.rb
@@ -73,12 +73,12 @@ class Item
 
   def log_sale
     File.open(@logfile, 'a') do |f|
-      f.write(nam.to_s + "\n")
+      f.write(name.to_s + "\n")
     end
   end
   private :log_sale
 
-  def nam
+  def name
     class_string = self.class.to_s
     short_class_string = class_string.sub(/^Item::/, '')
     lower_class_string = short_class_string.downcase
@@ -122,8 +122,8 @@ class Item
   end
 
   class CannedHaggis < Item
-    # the common-case implementation of Item.nam doesn't work here
-    def nam
+    # the common-case implementation of Item.name doesn't work here
+    def name
       :canned_haggis
     end
 
