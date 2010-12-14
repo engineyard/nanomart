92ee5be9bc53320a1d1c55dce4ca3e15d2807ab7
Tim Carey-Smith <tim@spork.in>
2010-12-14 10:37:52 -0800

    s/nam/name/g and refactored Item.name

--- nanomart.rb
+++ nanomart.rb
@@ -95,16 +95,12 @@ class Item
 
   def log_sale
     File.open(@logfile, 'a') do |f|
-      f.write(nam.to_s + "\n")
+      f.write(name.to_s + "\n")
     end
   end
 
-  def nam
-    class_string = self.class.to_s
-    short_class_string = class_string.sub(/^Item::/, '')
-    lower_class_string = short_class_string.downcase
-    class_sym = lower_class_string.to_sym
-    class_sym
+  def name
+    self.class.to_s.sub(/^Item::/, '').downcase.to_sym
   end
 
   def try_purchase(success)
@@ -142,8 +138,8 @@ class Item
   end
 
   class CannedHaggis < Item
-    # the common-case implementation of Item.nam doesn't work here
-    def nam
+    # the common-case implementation of Item.name doesn't work here
+    def name
       :canned_haggis
     end
 
