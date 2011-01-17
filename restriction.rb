4e0df0277cc5c639a591f996269930349d20a4de
Andy Delcambre <adelcambre@engineyard.com>
2011-01-17 12:54:14 -0800

    New Age general restriction.

--- restriction.rb
+++ restriction.rb
@@ -3,29 +3,14 @@ module Restriction
   DRINKING_AGE = 21
   SMOKING_AGE = 18
 
-  class DrinkingAge
-    def initialize(p)
-      @prompter = p
+  class Age
+    def initialize(age)
+      @age = age
     end
 
-    def ck
-      age = @prompter.get_age
-      if age >= DRINKING_AGE
-        true
-      else
-        false
-      end
-    end
-  end
-
-  class SmokingAge
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      age = @prompter.get_age
-      if age >= SMOKING_AGE
+    def ck(prompter)
+      age = prompter.get_age
+      if age >= @age
         true
       else
         false
@@ -34,11 +19,7 @@ module Restriction
   end
 
   class SundayBlueLaw
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
+    def ck(prompter)
       # pp Time.now.wday
       # debugger
       Time.now.wday != 0      # 0 is Sunday
