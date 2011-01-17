dc5f3b51cdb74031b15fd4cea885ad9527dbbc12
Andy Delcambre <adelcambre@engineyard.com>
2011-01-17 11:59:23 -0800

    Splitting files

---
+++ restriction.rb
@@ -0,0 +1,47 @@
+
+module Restriction
+  DRINKING_AGE = 21
+  SMOKING_AGE = 18
+
+  class DrinkingAge
+    def initialize(p)
+      @prompter = p
+    end
+
+    def ck
+      age = @prompter.get_age
+      if age >= DRINKING_AGE
+        true
+      else
+        false
+      end
+    end
+  end
+
+  class SmokingAge
+    def initialize(p)
+      @prompter = p
+    end
+
+    def ck
+      age = @prompter.get_age
+      if age >= SMOKING_AGE
+        true
+      else
+        false
+      end
+    end
+  end
+
+  class SundayBlueLaw
+    def initialize(p)
+      @prompter = p
+    end
+
+    def ck
+      # pp Time.now.wday
+      # debugger
+      Time.now.wday != 0      # 0 is Sunday
+    end
+  end
+end
