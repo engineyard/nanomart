b7d8c5046d85b254089bf1491fc1d91219cc1a82
Tim Carey-Smith <tim@spork.in>
2011-01-24 15:00:12 -0800

    Started changing architecture to accept multiple preferences for shoppers / sellers.

--- nanomart.rb
+++ nanomart.rb
@@ -5,22 +5,22 @@ require 'highline'
 class Nanomart
   class NoSale < StandardError; end
 
-  def initialize(logfile, prompter)
-    @logfile, @prompter = logfile, prompter
+  def initialize(logfile, preference_list)
+    @logfile, @preference_list = logfile, preference_list
   end
 
   def sell_me(itm_type)
     itm = case itm_type
           when :beer
-            Item::Beer.new(@logfile, @prompter)
+            Item::Beer.new(@logfile, @preference_list)
           when :whiskey
-            Item::Whiskey.new(@logfile, @prompter)
+            Item::Whiskey.new(@logfile, @preference_list)
           when :cigarettes
-            Item::Cigarettes.new(@logfile, @prompter)
+            Item::Cigarettes.new(@logfile, @preference_list)
           when :cola
-            Item::Cola.new(@logfile, @prompter)
+            Item::Cola.new(@logfile, @preference_list)
           when :canned_haggis
-            Item::CannedHaggis.new(@logfile, @prompter)
+            Item::CannedHaggis.new(@logfile, @preference_list)
           else
             raise ArgumentError, "Don't know how to sell #{itm_type}"
           end
@@ -28,16 +28,30 @@ class Nanomart
     itm.rstrctns.each do |r|
       itm.try_purchase(r.ck)
     end
+
     itm.log_sale
   end
 end
 
 class HighlinePrompter
-  def get_age
+  def get_restriction_value
     HighLine.new.ask('Age? ', Integer) # prompts for user's age, reads it in
+  end
+
+  def preference_type
+    :undef
   end
 end
 
+class AgePrompter
+  def get_restriction_value
+    HighLine.new.ask('Age? ', Integer) # prompts for user's age, reads it in
+  end
+
+  def preference_type
+    :age
+  end
+end
 
 module Restriction
   DRINKING_AGE = 21
@@ -45,42 +59,42 @@ module Restriction
 
   class DrinkingAge
     def initialize(p)
-      @prompter = p
+      @preference_list = p
     end
 
     def ck
-      age = @prompter.get_age
-      if age >= DRINKING_AGE
-        true
-      else
-        false
+      @preference_list.each do |preference|
+        if preference.preference_type == :age && preference.get_restriction_value >= DRINKING_AGE
+          return true
+        else
+          return false
+        end
       end
     end
   end
 
   class SmokingAge
     def initialize(p)
-      @prompter = p
+      @preference_list = p
     end
 
     def ck
-      age = @prompter.get_age
-      if age >= SMOKING_AGE
-        true
-      else
-        false
+      @preference_list.each do |preference|
+        if preference.preference_type == :age && preference.get_restriction_value >= SMOKING_AGE
+          return true
+        else
+          return false
+        end
       end
     end
   end
 
   class SundayBlueLaw
     def initialize(p)
-      @prompter = p
+      @preference_list = p
     end
 
     def ck
-      # pp Time.now.wday
-      # debugger
       Time.now.wday != 0      # 0 is Sunday
     end
   end
@@ -89,22 +103,20 @@ end
 class Item
   INVENTORY_LOG = 'inventory.log'
 
-  def initialize(logfile, prompter)
-    @logfile, @prompter = logfile, prompter
+  def initialize(logfile, preference_list)
+    @logfile, @preference_list = logfile, preference_list
   end
 
   def log_sale
     File.open(@logfile, 'a') do |f|
-      f.write(nam.to_s + "\n")
+      f.write(nam + "\n")
     end
   end
 
   def nam
     class_string = self.class.to_s
     short_class_string = class_string.sub(/^Item::/, '')
-    lower_class_string = short_class_string.downcase
-    class_sym = lower_class_string.to_sym
-    class_sym
+    short_class_string.downcase
   end
 
   def try_purchase(success)
@@ -117,21 +129,21 @@ class Item
 
   class Beer < Item
     def rstrctns
-      [Restriction::DrinkingAge.new(@prompter)]
+      [Restriction::DrinkingAge.new(@preference_list)]
     end
   end
 
   class Whiskey < Item
     # you can't sell hard liquor on Sundays for some reason
     def rstrctns
-      [Restriction::DrinkingAge.new(@prompter), Restriction::SundayBlueLaw.new(@prompter)]
+      [Restriction::DrinkingAge.new(@preference_list), Restriction::SundayBlueLaw.new(@preference_list)]
     end
   end
 
   class Cigarettes < Item
     # you have to be of a certain age to buy tobacco
     def rstrctns
-      [Restriction::SmokingAge.new(@prompter)]
+      [Restriction::SmokingAge.new(@preference_list)]
     end
   end
 
@@ -144,7 +156,7 @@ class Item
   class CannedHaggis < Item
     # the common-case implementation of Item.nam doesn't work here
     def nam
-      :canned_haggis
+      'canned_haggis'
     end
 
     def rstrctns
--- nanomart_spec.rb
+++ nanomart_spec.rb
@@ -4,38 +4,59 @@ require 'nanomart'
 
 
 class Age9
-  def get_age() 9 end
+  def get_restriction_value() 9 end
+
+  def preference_type
+    :age
+  end
 end
 
 class Age19
-  def get_age() 19 end
+  def get_restriction_value() 19 end
+
+  def preference_type
+    :age
+  end
 end
 
 class Age99
-  def get_age() 99 end
+  def get_restriction_value() 99 end
+
+  def preference_type
+    :age
+  end
 end
 
 describe "making sure the customer is old enough" do
   context "when you're a kid" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age9.new)
+      @nanomart = Nanomart.new('/dev/null', [Age9.new])
     end
 
-    it "lets you buy cola and canned haggis" do
+    it "lets you buy cola" do
       lambda { @nanomart.sell_me(:cola)          }.should_not raise_error
+    end
+
+    it "lets you buy canned haggis" do
       lambda { @nanomart.sell_me(:canned_haggis) }.should_not raise_error
     end
 
-    it "stops you from buying anything age-restricted" do
+    it "stops you from buying beer, which is age-restricted" do
       lambda { @nanomart.sell_me(:beer)       }.should raise_error(Nanomart::NoSale)
+    end
+
+    it "stops you from buying whiskey, which is age-restricted" do
       lambda { @nanomart.sell_me(:whiskey)    }.should raise_error(Nanomart::NoSale)
+    end
+
+    it "stops you from buying smokes, which are age-restricted" do
       lambda { @nanomart.sell_me(:cigarettes) }.should raise_error(Nanomart::NoSale)
     end
   end
 
   context "when you're a newly-minted adult" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age19.new)
+      @nanomart = Nanomart.new('/dev/null', [Age19.new])
     end
 
     it "lets you buy cola, canned haggis, and cigarettes (to hide the taste of the haggis)" do
@@ -52,7 +73,7 @@ describe "making sure the customer is old enough" do
 
   context "when you're an old fogey on Thursday" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age99.new)
+      @nanomart = Nanomart.new('/dev/null', [Age99.new])
       Time.stub!(:now).and_return(Time.local(2010, 8, 12, 12))  # Thursday Aug 12 2010 12:00
     end
 
@@ -67,7 +88,7 @@ describe "making sure the customer is old enough" do
 
   context "when you're an old fogey on Sunday" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age99.new)
+      @nanomart = Nanomart.new('/dev/null', [Age99.new])
       Time.stub!(:now).and_return(Time.local(2010, 8, 15, 12))  # Sunday Aug 15 2010 12:00
     end
 
