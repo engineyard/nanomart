4a85dbc9e594ea6756739b745be8582c9377ca27
Jason Hansen <jhansen@xmission.com>
2011-05-10 16:20:19 -0700

    cleanup and re-arranging

--- nanomart.rb
+++ nanomart.rb
@@ -24,60 +24,7 @@ class Nanomart
     itm.log_sale
   end
 end
-
-class HighlinePrompter
-  def get_age
-    HighLine.new.ask('Age? ', Integer) # prompts for user's age, reads it in
-  end
-end
-
-
-module Restriction
-  DRINKING_AGE = 21
-  SMOKING_AGE = 18
-
-  class DrinkingAge
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      age = @prompter.get_age
-      if age >= DRINKING_AGE
-        true
-      else
-        false
-      end
-    end
-  end
-
-  class SmokingAge
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      age = @prompter.get_age
-      if age >= SMOKING_AGE
-        true
-      else
-        false
-      end
-    end
-  end
-
-  class SundayBlueLaw
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      # pp Time.now.wday
-      # debugger
-      Time.now.wday != 0      # 0 is Sunday
-    end
-  end
-end
+require 'restriction'
 
 class Item
   INVENTORY_LOG = 'inventory.log'
