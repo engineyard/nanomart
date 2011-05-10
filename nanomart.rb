950797fce7d6f24df5e008261d3a4a1e7a76a4d8
Jason Hansen <jhansen@xmission.com>
2011-05-10 16:23:37 -0700

    more cleanup and re-arranging

--- nanomart.rb
+++ nanomart.rb
@@ -1,6 +1,6 @@
 # you can buy just a few things at this nanomart
 require 'highline'
-
+require 'item'
 
 class Nanomart
   class NoSale < StandardError; end
@@ -25,71 +25,3 @@ class Nanomart
   end
 end
 require 'restriction'
-
-class Item
-  INVENTORY_LOG = 'inventory.log'
-
-  def initialize(logfile, prompter)
-    @logfile, @prompter = logfile, prompter
-  end
-
-  def log_sale
-    File.open(@logfile, 'a') do |f|
-      f.write(nam.to_s + "\n")
-    end
-  end
-
-  def nam
-    class_string = self.class.to_s
-    short_class_string = class_string.sub(/^Item::/, '')
-    lower_class_string = short_class_string.downcase
-    class_sym = lower_class_string.to_sym
-    class_sym
-  end
-
-  def try_purchase(success)
-    if success
-      return true
-    else
-      raise Nanomart::NoSale
-    end
-  end
-
-  class Beer < Item
-    def restrictions
-      [Restriction::DrinkingAge.new(@prompter)]
-    end
-  end
-
-  class Whiskey < Item
-    # you can't sell hard liquor on Sundays for some reason
-    def restrictions
-      [Restriction::DrinkingAge.new(@prompter), Restriction::SundayBlueLaw.new(@prompter)]
-    end
-  end
-
-  class Cigarettes < Item
-    # you have to be of a certain age to buy tobacco
-    def restrictions
-      [Restriction::SmokingAge.new(@prompter)]
-    end
-  end
-
-  class Cola < Item
-    def restrictions
-      []
-    end
-  end
-
-  class CannedHaggis < Item
-    # the common-case implementation of Item.nam doesn't work here
-    def nam
-      :canned_haggis
-    end
-
-    def restrictions
-      []
-    end
-  end
-end
-
