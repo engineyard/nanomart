becb18459ab749df79c710965e846dbb863b8a65
Tammer Saleh <tsaleh@gmail.com>
2011-06-05 08:04:21 -0700

    Tammer's refactoring of Josh's solution

--- lib/nanomart.rb
+++ lib/nanomart.rb
@@ -1,36 +1,37 @@
 # you can buy just a few things at this nanomart
 require 'highline'
 
-
 class Nanomart
   class NoSale < StandardError; end
 
+  RESTRICTIONS = { :drinking => lambda{|age| age >= 21},
+                   :smoking  => lambda{|age| age >= 18},
+                   :blue_law => lambda{|age| Time.now.wday != 0} }
+
+  ITEM_RESTRICTIONS = { :beer          => [RESTRICTIONS[:drinking]],
+                        :whiskey       => [RESTRICTIONS[:drinking], RESTRICTIONS[:blue_law]],
+                        :cigarettes    => [RESTRICTIONS[:smoking]],
+                        :cola          => [],
+                        :canned_haggis => [] }
   attr_reader :age
   
   def initialize(age)
     @age = age
   end
   
-  def self.items
-    @@items = {
-      :beer => [restrictions[:drinking]],
-      :whiskey => [restrictions[:drinking], restrictions[:blue_law]],
-      :cigarettes => [restrictions[:smoking]],
-      :cola => [],
-      :canned_haggis => []
-    }
+  def have_item?(item)
+    Nanomart::ITEM_RESTRICTIONS.key?(item)
   end
 
-  def self.restrictions
-    @@restrictions = {
-      :drinking => lambda{|age| age >= 21},
-      :smoking => lambda{|age| age >= 18},
-      :blue_law => lambda{|age| Time.now.wday != 0}
-    }
+  def allowed_to_buy?(item)
+    Nanomart::ITEM_RESTRICTIONS[item].all? do |restriction| 
+      restriction.call(age)
+    end
   end
 
-  def sell_me(item_type)
-    raise ArgumentError, "Don't know how to sell #{item_type}" unless Nanomart.items.key?(item_type)
-    raise Nanomart::NoSale unless Nanomart.items[item_type].inject(true){|r,e| r && e.call(@age)}
+  def sell_me(item)
+    raise ArgumentError, "Don't have #{item}." unless have_item?(item)
+    raise Nanomart::NoSale                     unless allowed_to_buy?(item)
   end
 end
+
