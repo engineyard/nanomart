845ce49aee619b237b206d700d3b30a4d6b6ef97
Jason Hansen <jhansen@engineyard.com>
2011-08-29 14:13:30 -0700

    Buy.rb: test harness for making sure highline works

---
+++ buy.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+
+require 'rubygems'
+require 'bundler/setup'
+require File.expand_path(File.join(File.dirname(__FILE__), 'nanomart'))
+
+
+logfile = "/tmp/purchases.log"
+nanomart = Nanomart.new(logfile, HighlinePrompter.new)
+
+nanomart.sell_me(:beer)
