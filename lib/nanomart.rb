d35769b7004820dd6f998207fb361c72c9e1ce29
Josh Lane <me@joshualane.com>
2012-08-29 16:35:07 -0700

    chad

--- lib/nanomart.rb
+++ lib/nanomart.rb
@@ -1,5 +1,6 @@
 # you can buy just a few things at this nanomart
 require 'highline'
+require 'Item' # +1 for chad
 
 
 class Nanomart
@@ -9,6 +10,7 @@ class Nanomart
     @logfile, @prompter = logfile, prompter
   end
 
+  # things I can buy
   def sell_me(itm_type)
     itm = case itm_type
           when :beer
@@ -82,73 +84,6 @@ module Restriction
       # pp Time.now.wday
       # debugger
       Time.now.wday != 0      # 0 is Sunday
-    end
-  end
-end
-
-class Item
-  INVENTORY_LOG = 'inventory.log'
-
-  def initialize(logfile, prompter)
-    @logfile, @prompter = logfile, prompter
-  end
-
-  def log_sale
-    File.open(@logfile, 'a') do |f|
-      f.write(nam.to_s + "\n")
-    end
-  end
-
-  def nam
-    class_string = self.class.to_s
-    short_class_string = class_string.sub(/^Item::/, '')
-    lower_class_string = short_class_string.downcase
-    class_sym = lower_class_string.to_sym
-    class_sym
-  end
-
-  def try_purchase(success)
-    if success
-      return true
-    else
-      raise Nanomart::NoSale
-    end
-  end
-
-  class Beer < Item
-    def rstrctns
-      [Restriction::DrinkingAge.new(@prompter)]
-    end
-  end
-
-  class Whiskey < Item
-    # you can't sell hard liquor on Sundays for some reason
-    def rstrctns
-      [Restriction::DrinkingAge.new(@prompter), Restriction::SundayBlueLaw.new(@prompter)]
-    end
-  end
-
-  class Cigarettes < Item
-    # you have to be of a certain age to buy tobacco
-    def rstrctns
-      [Restriction::SmokingAge.new(@prompter)]
-    end
-  end
-
-  class Cola < Item
-    def rstrctns
-      []
-    end
-  end
-
-  class CannedHaggis < Item
-    # the common-case implementation of Item.nam doesn't work here
-    def nam
-      :canned_haggis
-    end
-
-    def rstrctns
-      []
     end
   end
 end
