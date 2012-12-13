0911e26f2bc3698f045ccc3e6f7e7fdf797025a8
James Rucker & Ryan Souza <jrucker@engineyard.com>
2012-12-13 11:51:11 -0800

    use mapping for item names to item type classes

--- nanomart.rb
+++ nanomart.rb
@@ -2,90 +2,6 @@
 require 'highline'
 
 
-class Nanomart
-  class NoSale < StandardError; end
-
-  def initialize(logfile, prompter)
-    @logfile, @prompter = logfile, prompter
-  end
-
-  def sell_me(itm_type)
-    itm = case itm_type
-          when :beer
-            Item::Beer.new(@logfile, @prompter)
-          when :whiskey
-            Item::Whiskey.new(@logfile, @prompter)
-          when :cigarettes
-            Item::Cigarettes.new(@logfile, @prompter)
-          when :cola
-            Item::Cola.new(@logfile, @prompter)
-          when :canned_haggis
-            Item::CannedHaggis.new(@logfile, @prompter)
-          else
-            raise ArgumentError, "Don't know how to sell #{itm_type}"
-          end
-
-    itm.rstrctns.each do |r|
-      itm.try_purchase(r.ck)
-    end
-    itm.log_sale
-  end
-end
-
-class HighlinePrompter
-  def get_age
-    HighLine.new.ask('Age? ', Integer) # prompts for user's age, reads it in
-  end
-end
-
-
-module Restriction
-  DRINKING_AGE = 21
-  SMOKING_AGE = 18
-
-  class DrinkingAge
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      age = @prompter.get_age
-      if age >= DRINKING_AGE
-        true
-      else
-        false
-      end
-    end
-  end
-
-  class SmokingAge
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      age = @prompter.get_age
-      if age >= SMOKING_AGE
-        true
-      else
-        false
-      end
-    end
-  end
-
-  class SundayBlueLaw
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      # pp Time.now.wday
-      # debugger
-      Time.now.wday != 0      # 0 is Sunday
-    end
-  end
-end
-
 class Item
   INVENTORY_LOG = 'inventory.log'
 
@@ -153,3 +69,86 @@ class Item
   end
 end
 
+
+class Nanomart
+  class NoSale < StandardError; end
+
+  ITEM_MAPS = {
+    :beer => Item::Beer,
+    :whiskey => Item::Whiskey,
+    :cola => Item::Cola,
+    :canned_haggis => Item::CannedHaggis,
+    :cigarettes => Item::Cigarettes
+  }
+
+  def initialize(logfile, prompter)
+    @logfile, @prompter = logfile, prompter
+  end
+
+  def sell_me(item_type)
+    if type = ITEM_MAPS[item_type]
+      item = type.new(@logfile, @prompter)
+    else
+      raise ArgumentError, "Don't know how to sell #{item_type}"
+    end
+
+    item.rstrctns.each do |r|
+      item.try_purchase(r.ck)
+    end
+    item.log_sale
+  end
+end
+
+class HighlinePrompter
+  def get_age
+    HighLine.new.ask('Age? ', Integer) # prompts for user's age, reads it in
+  end
+end
+
+
+module Restriction
+  DRINKING_AGE = 21
+  SMOKING_AGE = 18
+
+  class DrinkingAge
+    def initialize(p)
+      @prompter = p
+    end
+
+    def ck
+      age = @prompter.get_age
+      if age >= DRINKING_AGE
+        true
+      else
+        false
+      end
+    end
+  end
+
+  class SmokingAge
+    def initialize(p)
+      @prompter = p
+    end
+
+    def ck
+      age = @prompter.get_age
+      if age >= SMOKING_AGE
+        true
+      else
+        false
+      end
+    end
+  end
+
+  class SundayBlueLaw
+    def initialize(p)
+      @prompter = p
+    end
+
+    def ck
+      # pp Time.now.wday
+      # debugger
+      Time.now.wday != 0      # 0 is Sunday
+    end
+  end
+end
